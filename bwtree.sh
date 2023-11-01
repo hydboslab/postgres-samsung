defaf04be91814bf790c79effc6b40dd7129d763
kyungminlim <kyungminlim@hanyang.ac.kr>
2023-11-01 16:27:15 +0900

    add bwtree.sh

---
+++ bwtree.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ../
+
+git clone https://github.com/cmu-db/noisepage.git
+
+cd noisepage
+sudo ./script/installation/packages.sh
+
+mkdir build
+cd build
+
+cmake -GNinja \
+-DCMAKE_BUILD_TYPE=Release \
+-DNOISEPAGE_USE_JEMALLOC=ON \
+-DNOISEPAGE_UNITY_BUILD=ON \
+-DNOISEPAGE_BUILD_BENCHMARKS=ON \
+-DNOISEPAGE_BUILD_TESTS=ON \
+-DNOISEPAGE_UNITTEST_OUTPUT_ON_FAILURE=ON ..
+
+ninja noisepage
+
+./bin/noisepage
+
+cd postgres-samsung
